4fb28e150893268558e451afaadc6b51d4d24b76
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-13 16:57:09 +0530

    score

--- js/gameover.js
+++ js/gameover.js
@@ -22,7 +22,7 @@ saveHighScore = e => {
     name: username.value
   };
   highScores.push(score);
-  highScores.sort((a, b) => b.score - a.score);
+
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
