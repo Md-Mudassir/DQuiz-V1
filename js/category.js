55cce18598760791ab901cc9a8e09458a9ffe77a
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-13 19:16:49 +0530

    updates

--- js/category.js
+++
@@ -1,8 +0,0 @@
-let valuess = "General Knowlegde";
-
-let hello = localStorage.getItem("storageName");
-
-window.onload = function() {
-  console.log("h");
-  valuess === hello ? hello : valuess;
-};
